0d90d502eb05f81e468b19ca0a0b10db77183ec3
Guy Mesika <guy@coti.io>
2024-05-22 11:35:03 +0300

    fix rpc url

--- src/provider.ts
+++ src/provider.ts
@@ -1,5 +1,5 @@
 import { JsonRpcProvider } from "ethers"
 
 export function getDefaultProvider() {
-  return new JsonRpcProvider("https://devnet.coti.io")
+  return new JsonRpcProvider("https://devnet.coti.io/rpc")
 }
