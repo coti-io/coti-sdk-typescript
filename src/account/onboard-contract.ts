aa09c58ae2382cb07c1057523d9d54ef18d7b827
Spencer Miller <spencer.github.123@gmail.com>
2024-05-22 12:04:22 +0300

    updated onboard contract address and ABI

--- src/account/onboard-contract.ts
+++ src/account/onboard-contract.ts
@@ -1,46 +1,40 @@
-export const ONBOARD_CONTRACT_ADDRESS = "0x9E6987C8fD552d90c252B7fe4aF2387D47f928AE"
+export const ONBOARD_CONTRACT_ADDRESS = "0x5D49C3F49F19dc4e257975Fb1CcE057b3796f5F1"
 export const ONBOARD_CONTRACT_ABI = [
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "_from",
-        type: "address",
+        "indexed": true,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
       },
       {
-        indexed: false,
-        internalType: "bytes",
-        name: "userKey",
-        type: "bytes",
-      },
+        "indexed": false,
+        "internalType": "bytes",
+        "name": "userKey",
+        "type": "bytes"
+      }
     ],
-    name: "AccountOnboarded",
-    type: "event",
+    "name": "AccountOnboarded",
+    "type": "event"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "bytes",
-        name: "signedEK",
-        type: "bytes",
+        "internalType": "bytes",
+        "name": "signedEK",
+        "type": "bytes"
       },
       {
-        internalType: "bytes",
-        name: "signature",
-        type: "bytes",
-      },
+        "internalType": "bytes",
+        "name": "signature",
+        "type": "bytes"
+      }
     ],
-    name: "OnboardAccount",
-    outputs: [
-      {
-        internalType: "bytes",
-        name: "accountKey",
-        type: "bytes",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
+    "name": "OnboardAccount",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ]
