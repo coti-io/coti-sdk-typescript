43c66ad9dea214e793856e4904d07d5621df571e
Spencer Miller <spencer.github.123@gmail.com>
2024-07-01 14:44:41 +0300

    added support for string encryption

--- src/account/confidential-account.ts
+++ src/account/confidential-account.ts
@@ -1,5 +1,5 @@
-import { BaseWallet, Wallet, Contract, Provider, JsonRpcProvider } from "ethers"
-import { decryptValue, prepareIT } from "../libs/crypto"
+import { BaseWallet, Wallet, Contract, Provider } from "ethers"
+import { decryptString, decryptUint, prepareStringIT, prepareUintIT } from "../libs/crypto"
 import { getDefaultProvider } from "../provider"
 import { onboard } from "./onboard"
 
@@ -7,11 +7,11 @@ export class ConfidentialAccount {
   constructor(readonly wallet: BaseWallet, readonly userKey: string) {}
 
   public decryptValue(ciphertextValue: bigint) {
-    return decryptValue(ciphertextValue, this.userKey)
+    return decryptUint(ciphertextValue, this.userKey)
   }
 
   public encryptValue(plaintextValue: bigint | number, contractAddress: string, functionSelector: string) {
-    return prepareIT(BigInt(plaintextValue), this, contractAddress, functionSelector)
+    return prepareUintIT(BigInt(plaintextValue), this, contractAddress, functionSelector)
   }
 
   public static async onboard(wallet: BaseWallet, contract?: Contract): Promise<ConfidentialAccount> {
