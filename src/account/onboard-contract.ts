6b286257626a50d913292068d93f349d29433b24
Guy Mesika <guy@coti.io>
2024-07-28 17:06:27 +0300

    update AccountOnboard contract address and ABI

--- src/account/onboard-contract.ts
+++ src/account/onboard-contract.ts
@@ -1,40 +1,40 @@
-export const ONBOARD_CONTRACT_ADDRESS = "0xadb3952a07B2c9fA7dbd2F583cd2F0412C97eC33"
+export const ONBOARD_CONTRACT_ADDRESS = "0x413370ed41FB9EE3aea0B1B91FD336cC0be1Bad6"
 export const ONBOARD_CONTRACT_ABI = [
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "_from",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "bytes",
-                "name": "userKey",
-                "type": "bytes"
-            }
-        ],
-        "name": "AccountOnboarded",
-        "type": "event"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "bytes",
-                "name": "signedEK",
-                "type": "bytes"
-            },
-            {
-                "internalType": "bytes",
-                "name": "signature",
-                "type": "bytes"
-            }
-        ],
-        "name": "OnboardAccount",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bytes",
+        "name": "userKey",
+        "type": "bytes"
+      }
+    ],
+    "name": "AccountOnboarded",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "publicKey",
+        "type": "bytes"
+      },
+      {
+        "internalType": "bytes",
+        "name": "signedEK",
+        "type": "bytes"
+      }
+    ],
+    "name": "onboardAccount",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ]
